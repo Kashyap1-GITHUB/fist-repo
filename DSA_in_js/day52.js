a8b20dfd71f901a4485ee0e23b92d48ac86d55ac
Kashyap1-GITHUB <baghelkashyap2004@gmail.com>
2025-04-13 16:33:11 +0530

    check if two String are Anagarams

--- DSA_in_js/day52.js
+++ DSA_in_js/day52.js
@@ -1,3 +1,4 @@
+/*
 const prompt = require("prompt-sync")();
 
 let s = prompt("Enter String :");
@@ -14,3 +15,39 @@ for (let j = 0; j < arr.length; j++) {
     console.log(char + " : " + arr[j]);
   }
 }
+  */
+
+const prompt = require("prompt-sync")();
+
+let s1 = prompt("Enter String one:");
+let s2 = prompt("Enter String two:");
+let arr = new Array(123).fill(0);
+let Anagaram = true
+
+if(s1.length != s2.length){
+    console.log("this String is not Anagaram");
+}
+else{
+    
+for (let i = 0; i < s1.length; i++) {
+    let escii1 = s1.charCodeAt(i);
+    arr[escii1] += 1;
+  }
+  
+  for (let i = 0; i < s2.length; i++) {
+    let escii2 = s2.charCodeAt(i);
+    arr[escii2] -= 1;
+  }
+  
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j] > 0) {
+      Anagaram = false
+      break;
+    }
+  }
+  
+  if(Anagaram) console.log("this String is Anagaram");
+  else console.log("this String is Not Anagaram");
+}
+
+
