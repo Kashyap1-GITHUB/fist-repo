c364ee6b23973183309b50e3779b2d8327ced1f1
Kashyap1-GITHUB <baghelkashyap2004@gmail.com>
2025-04-21 00:38:23 +0530

    and operatar

--- DSA_in_js/day55.js
+++ DSA_in_js/day55.js
@@ -1,1 +1,33 @@
-console.log(13<<2);
+// console.log(13<<2);
+
+const PromptSync = require("prompt-sync");
+
+/*
+let a = 10 ;
+let b = 5;
+
+a = a^b;
+b = a^b;
+a = a^b;
+
+console.log(a,b);
+*/
+/*
+
+let n = 32;
+
+console.log((n&1 )=== 0 ?"even":"odd");
+
+*/
+
+
+var prompt = require('prompt-sync')();
+let n = prompt("Eenter number for chack power of two or not :")
+
+if((n&(n-1))=== 0){
+    console.log("this number power of 2 :",n);
+}
+else{
+    console.log("not power of 2");
+
+}
