4f21e22ef0f9c8ff1c6f30ad3aac7f5f84cf50f3
Kashyap1-GITHUB <baghelkashyap2004@gmail.com>
2025-04-22 00:47:55 +0530

    recursion prectice

---
+++ DSA_in_js/day56.js
@@ -0,0 +1,11 @@
+const Prompt = require("prompt-sync")();
+ 
+let num = Number(Prompt("Enter number :"));
+
+// kuch(num);
+// function kuch(a){
+//     if(a === 0) return
+//     console.log(a);
+//     kuch(a-1)
+// }
+
